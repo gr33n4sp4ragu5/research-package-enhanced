35495ad9c6e07c0c12fd3e525a713661b8969583
AlbertoHurtado <=>
2021-03-08 20:50:10 +0100

    Add an image in the example package and render it in the choice questions

--- lib/src/ui/QuestionBody/RPUIChoiceQuestionBody.dart
+++ lib/src/ui/QuestionBody/RPUIChoiceQuestionBody.dart
@@ -76,6 +76,7 @@ class _RPUIChoiceQuestionBodyState extends State<RPUIChoiceQuestionBody>
       mainAxisSize: MainAxisSize.min,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
+        Image.asset('assets/images/owl-3.jpg'),
         Padding(
           padding: const EdgeInsets.all(8.0),
           child: Text(widget._answerFormat.answerStyle == ChoiceAnswerStyle.MultipleChoice
