95eb5f26ad9042099c9e05e2f8c9070a9e14ba78
AlbertoHurtado <a.hurtado@alumnos.upm.es>
2021-03-28 19:39:50 +0200

    Simplified survey

--- example/lib/research_package_objects/linear_survey_objects.dart
+++ example/lib/research_package_objects/linear_survey_objects.dart
@@ -10,32 +10,29 @@ List<RPChoice> timeChoices = [
   RPChoice.withParams("At no time", 0),
 ];
 
-List<RPChoice> joyfulActivities = [
-  RPChoice.withParams("Playing games", 6, true),
-  RPChoice.withParams("Jogging", 5),
-  RPChoice.withParams("Playing an instrument", 4),
-  RPChoice.withParams("Family and friends", 3),
-  RPChoice.withParams("Doing sports", 2),
-  RPChoice.withParams("Reading", 1),
-  RPChoice.withParams("Being productive", 0),
+List<AlbertoChoice> timeChoicesAlberto = [
+  AlbertoChoice.withParams("All of the time", 5),
+  AlbertoChoice.withParams("Most of the time", 4),
+  AlbertoChoice.withParams("More than half of the time", 3),
+  AlbertoChoice.withParams("Less than half of the time", 2),
+  AlbertoChoice.withParams("Some of the time", 1),
+  AlbertoChoice.withParams("At no time", 0),
 ];
 
-List<RPChoice> instruments = [
-  RPChoice.withParams("Guitar", 3),
-  RPChoice.withParams("Piano", 2),
-  RPChoice.withParams("Saxophone", 1),
-];
-
-List<RPImageChoice> images = [
-  RPImageChoice.withParams(Image.asset('assets/images/very-sad.png'), 0, 'Feeling very sad'),
-  RPImageChoice.withParams(Image.asset('assets/images/sad.png'), 0, 'Feeling sad'),
-  RPImageChoice.withParams(Image.asset('assets/images/ok.png'), 0, 'Feeling ok'),
-  RPImageChoice.withParams(Image.asset('assets/images/happy.png'), 0, 'Feeling happy'),
-  RPImageChoice.withParams(Image.asset('assets/images/very-happy.png'), 0, 'Feeling very happy'),
+List<AlbertoChoice> joyfulActivities = [
+  AlbertoChoice.withParams("Playing games", 6, true),
+  AlbertoChoice.withParams("Jogging", 5),
+  AlbertoChoice.withParams("Playing an instrument", 4),
+  AlbertoChoice.withParams("Family and friends", 3),
+  AlbertoChoice.withParams("Doing sports", 2),
+  AlbertoChoice.withParams("Reading", 1),
+  AlbertoChoice.withParams("Being productive", 0),
 ];
 
 RPChoiceAnswerFormat timeAnswerFormat =
     RPChoiceAnswerFormat.withParams(ChoiceAnswerStyle.SingleChoice, timeChoices, 'assets/images/owl-3.jpg');
+AlbertoAnswerFormat timeAnswerFormatAlberto =
+AlbertoAnswerFormat.withParams(ChoiceAnswerStyle.SingleChoice, timeChoicesAlberto, 'assets/images/owl-3.jpg');
 // All types of DateTime answer formats
 RPDateTimeAnswerFormat timeOfDayAnswerFormat =
     RPDateTimeAnswerFormat.withParams(DateTimeAnswerStyle.TimeOfDay);
@@ -43,47 +40,9 @@ RPDateTimeAnswerFormat dateAndTimeAnswerFormat =
     RPDateTimeAnswerFormat.withParams(DateTimeAnswerStyle.DateAndTime);
 RPDateTimeAnswerFormat dateAnswerFormat = RPDateTimeAnswerFormat.withParams(DateTimeAnswerStyle.Date);
 
-// Slider
-RPSliderAnswerFormat sliderAnswerFormat =
-    RPSliderAnswerFormat.withParams(0, 100, divisions: 10, prefix: "\$", suffix: " paid");
+AlbertoAnswerFormat joyfulActivitiesAnswerFormat =
+    AlbertoAnswerFormat.withParams(ChoiceAnswerStyle.MultipleChoice, joyfulActivities, 'assets/images/waving-hand.png');
 
-RPChoiceAnswerFormat joyfulActivitiesAnswerFormat =
-    RPChoiceAnswerFormat.withParams(ChoiceAnswerStyle.MultipleChoice, joyfulActivities, 'assets/images/waving-hand.png');
-
-RPChoiceAnswerFormat instrumentsAnswerFormat =
-    RPChoiceAnswerFormat.withParams(ChoiceAnswerStyle.MultipleChoice, instruments, 'assets/images/owl-3.jpg');
-
-RPIntegerAnswerFormat weightIntegerAnswerFormat = RPIntegerAnswerFormat.withParams(0, 200, "KG");
-
-RPBooleanAnswerFormat smokingBooleanAnswerFormat =
-    RPBooleanAnswerFormat.withParams("Yes, absolutely", "No, never");
-
-RPIntegerAnswerFormat minutesIntegerAnswerFormat = RPIntegerAnswerFormat.withParams(0, 10000, "minutes");
-
-RPTextAnswerFormat textAnswerFormat = RPTextAnswerFormat.withParams("Write your answer here");
-
-RPImageChoiceAnswerFormat imageChoiceAnswerFormat = RPImageChoiceAnswerFormat.withParams(images);
-
-RPQuestionStep additionalInfoQuestionStep = RPQuestionStep.withAnswerFormat(
-    'additionalInfoQuestionStepID', 'Do you have any more details you would like to add?', textAnswerFormat,
-    optional: true);
-
-RPQuestionStep timeOfDayQuestionStep = RPQuestionStep.withAnswerFormat(
-    'timeOfDayQuestionStepID', 'When did you wake up?', timeOfDayAnswerFormat);
-
-RPQuestionStep dateAndTimeQuestionStep = RPQuestionStep.withAnswerFormat(
-    'dateAndTimeQuestionStepID', 'When did you last eat unhealthy food?', dateAndTimeAnswerFormat);
-
-RPQuestionStep dateQuestionStep = RPQuestionStep.withAnswerFormat(
-    'dateQuestionStepID', 'When did you last drink alcohol?', dateAnswerFormat);
-
-// Slider
-RPQuestionStep sliderQuestionStep = RPQuestionStep.withAnswerFormat(
-  "sliderQuestionsStepID",
-  "What did you pay for insulin?",
-  sliderAnswerFormat,
-  optional: true,
-);
 
 RPQuestionStep singleChoiceQuestionStep = RPQuestionStep.withAnswerFormat(
   "questionStep1ID",
@@ -91,36 +50,15 @@ RPQuestionStep singleChoiceQuestionStep = RPQuestionStep.withAnswerFormat(
   timeAnswerFormat,
 );
 
-RPQuestionStep smokingQuestionStep = RPQuestionStep.withAnswerFormat(
-  "booleanQuestionStepID",
-  "Do you smoke?",
-  smokingBooleanAnswerFormat,
+RPQuestionStep singleChoiceQuestionStepAlberto = RPQuestionStep.withAnswerFormat(
+  "questionStep1ID",
+  "I have felt cheerful and in good spirits",
+  timeAnswerFormatAlberto,
 );
-
-RPQuestionStep instrumentChoiceQuestionStep = RPQuestionStep.withAnswerFormat(
-    "instrumentChoiceQuestionStepID", "Which instrument are you playing?", instrumentsAnswerFormat);
 
 RPQuestionStep happinessChoiceQuestionStep = RPQuestionStep.withAnswerFormat(
     "happinessChoiceQuestionStepID", "What makes you happy?", joyfulActivitiesAnswerFormat);
 
-RPQuestionStep weightQuestionStep = RPQuestionStep.withAnswerFormat(
-    "weightQuestionStepID", "What is your weight?", weightIntegerAnswerFormat);
-
-RPQuestionStep minutesQuestionStep = RPQuestionStep.withAnswerFormat(
-    "minutesQuestionStepID", "How many minutes do you spend practicing a week?", minutesIntegerAnswerFormat);
-
-RPQuestionStep imageChoiceQuestionStep = RPQuestionStep.withAnswerFormat(
-  "imageStepID",
-  "Indicate you mood by selecting a picture!",
-  imageChoiceAnswerFormat,
-);
-
-RPFormStep formStep = RPFormStep.withTitle(
-  "formstepID",
-  [instrumentChoiceQuestionStep, minutesQuestionStep, dateQuestionStep],
-  "Questions about music",
-  optional: true,
-);
 
 RPCompletionStep completionStep = RPCompletionStep("completionID")
   ..title = "Finished"
@@ -135,9 +73,8 @@ RPOrderedTask linearSurveyTask = RPOrderedTask(
   "surveyTaskID",
   [
     singleChoiceQuestionStep,
+    singleChoiceQuestionStepAlberto,
     happinessChoiceQuestionStep,
-    weightQuestionStep,
-    additionalInfoQuestionStep,
     completionStep
   ],
 );
